366936ca89c69615440fdc14f599de81f844029e
Rean Glenn A. Roquero <reanglenn08@gmail.com>
2025-06-06 13:47:40 +0800

    Encapsulate the slider output implementation

--- controls/SliderController.js
+++ controls/SliderController.js
@@ -2,10 +2,13 @@ import * as THREE from 'three'
 import Draw from '../render/draw.js'
 
 class SliderController {
-    constructor(slider, renderer, camera) {
+    constructor(slider, renderer, camera, chargeConfig, chargeMeshes, scene) {
         this.slider = slider;
         this.renderer = renderer;
         this.camera = camera;
+        this.chargeConfig = chargeConfig;
+        this.chargeMeshes = chargeMeshes;
+        this.scene = scene;
         this.isVisible = false;
         this.activeMesh = null;
         this.activeMeshIndex = -1;
@@ -47,6 +50,20 @@ class SliderController {
         const val = parseFloat(this.slider.value);
         let color = val > 0 ? '#ff3366' : (val < 0 ? '#3366ff' : '#888');
         this.slider.style.setProperty('--thumb-color', color);
+    }
+    
+    updateCharge(value) {
+        if (this.activeMeshIndex < 0) return;
+        const newCharge = parseFloat(value);
+        this.chargeConfig.charges[this.activeMeshIndex].charge = newCharge;
+        const updatedMesh = this.chargeConfig.charges[this.activeMeshIndex].generateMesh();
+        updatedMesh.userData.index = this.activeMeshIndex;
+        this.scene.remove(this.activeMesh);
+        this.chargeMeshes[this.activeMeshIndex] = updatedMesh;
+        this.activeMesh = updatedMesh;
+        this.scene.add(updatedMesh);
+        this.updateThumbColor();
+        Draw.drawFields(this.scene, this.chargeConfig, true);
     }
 }
 
